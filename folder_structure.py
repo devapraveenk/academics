b828c5123a9bfd18d23630d7630e1e759f80b560
devapraveenk <devapraveen20@gmail.com>
2024-10-11 23:24:09 +0530

    ResNet50

---
+++ folder_structure.py
@@ -0,0 +1,25 @@
+import os
+
+def print_directory_tree(startpath, indent_level=0, include_first_level=None, exclude_folders=None):
+ # Include only these in the first iteration
+    
+    if exclude_folders is None:
+        exclude_folders = {'.git', '__pycache__', 'image','.DS_Store'}  # Exclude these folders in all iterations
+    
+    indent = ' ' * (4 * indent_level)  # Indentation for nested folders
+    print(indent + os.path.basename(startpath) + '/')
+    
+    with os.scandir(startpath) as entries:
+        for entry in entries:
+            
+            # Skip excluded folders
+            if entry.is_dir(follow_symlinks=False) and entry.name not in exclude_folders:
+                print_directory_tree(entry.path, indent_level + 1, include_first_level, exclude_folders)
+            elif entry.is_file(follow_symlinks=False):
+                print(indent + ' ' * 4 + entry.name)
+
+# Set the path to your project folder
+project_path = os.path.abspath('.')
+
+# Call the function to print the directory structure, including only `path_a` and `path_b` in the first iteration
+print_directory_tree(project_path)
